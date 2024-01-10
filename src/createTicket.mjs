b351e5be461e35706084ead8a1d5fe1c4f9f2e0d
Jaz Singh <jazeee@gmail.com>
2024-01-09 18:57:20 -0800

    Add prompting and update createIssue

---
+++ src/createTicket.mjs
@@ -0,0 +1,42 @@
+import { LinearClient } from '@linear/sdk'
+
+// Api key authentication
+const linearClient = new LinearClient({
+  apiKey: process.env.CBH_LINEAR_API_KEY
+});
+
+// async function getMyIssues() {
+//   const me = await linearClient.viewer;
+//   const myIssues = await me.assignedIssues();
+
+//   if (myIssues.nodes.length) {
+//     myIssues.nodes.map(issue => console.log(`${me.displayName} has issue: ${issue.title}`));
+//   } else {
+//     console.log(`${me.displayName} has no issues`);
+//   }
+// }
+
+// getMyIssues();
+async function getTeamByKey(key = "FEF") {
+  const teams = await linearClient.teams({ filter: {
+    key: {
+      eq: key,
+    },
+  }});
+  return teams.nodes[0];
+}
+
+export async function createIssue(props) {
+  const team = await getTeamByKey("FEF");
+  const me = await linearClient.viewer;
+  if (team.id && me.id) {
+    const response = await linearClient.createIssue({ teamId: team.id, ...props, assigneeId: me.id });
+    const issue = await response.issue;
+    return {
+      response,
+      issue,
+      url: issue?.url ?? "", // e.g. https://linear.app/clipboardhealth/issue/FEF-467/test-can-delete
+      identifier: issue?.identifier ?? "", // e.g. FEF-467
+    }
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,33 @@
+import { createIssue } from "./createTicket.mjs";
+import { editor, input } from '@inquirer/prompts';
+
+async function editAndCreateTicket(props) {
+  const { isPermanentTicket = true } = props;
+  const title = await input({ message: 'Title' });
+  const summary = (await editor({ message: 'Summary', default: "Summary\n===\n", waitForUseInput: false })).trim();
+  const changes = (await editor({ message: 'Changes', default: "Changes\n===\n", waitForUseInput: false })).trim();
+
+  console.log("Creating ticket...");
+  const { issue, url, identifier } = await createIssue({ title, description: `
+${summary}
+${changes}
+` });
+
+  console.log(`
+fix(${identifier}): ${title}
+
+${summary}
+
+Ticket:
+${url}
+
+${changes}`);
+
+  if (!isPermanentTicket) {
+    await issue.delete();
+  }
+}
+
+editAndCreateTicket({
+  isPermanentTicket: true,
+});
