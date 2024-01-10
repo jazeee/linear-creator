c44e024a47bedc8c0ecd9caf80eec5cda9972ed6
Jaz Singh <jazeee@gmail.com>
2024-01-10 08:53:19 -0800

    Adjust ticket creation

--- src/main.js
+++ src/main.js
@@ -4,13 +4,12 @@ import { editor, input } from '@inquirer/prompts';
 async function editAndCreateTicket(props) {
   const { isPermanentTicket = true } = props;
   const title = await input({ message: 'Title' });
-  const summary = (await editor({ message: 'Summary', default: "Summary\n===\n", waitForUseInput: false })).trim();
+  const summary = (await editor({ message: 'Summary', default: "Summary\n===\n\n", waitForUseInput: false })).trim();
   const changes = (await editor({ message: 'Changes', default: "Changes\n===\n", waitForUseInput: false })).trim();
 
   console.log("Creating ticket...");
   const { issue, url, identifier } = await createIssue({ title, description: `
 ${summary}
-${changes}
 ` });
 
   console.log(`
